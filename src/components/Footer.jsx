57d48dcc9c942ee82eadfb5837d47d8db4977897
shin <87216653+tippytaptip66@users.noreply.github.com>
2026-09-06 21:11:00 +0800

    Update footer wordmark to 'By Bacoro'

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="footer">
-      <div className="wordmark">TECHWEEK</div>
+      <div className="wordmark">By Bacoro</div>
       <p className="copyright">College of Computer Studies · St. Timothy University</p>
       <div className="divider" />
       <div className="socials">
